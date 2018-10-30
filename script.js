a70ffce9bc95f4fdff810651f5baedd18fd4942a
Danny Trumble <dtrumble12@gmail.com>
2018-10-29 23:06:49 -0500

    Added the front-end

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ function palindrome(str) {
   let newStr =  str.replace(/[^a-z0-9]/gmi, "").toLowerCase();
   let reverseStr = str.split("").reverse().join("").replace(/[^a-z0-9]/gmi, "").toLowerCase();
   if (newStr === reverseStr) {
-    return true
+    return document.getElementById("answer").innerHTML = "true";
   }
 
   else {
-    return false;
+    return document.getElementById("answer").innerHTML = "false";
   }
 
   
-}
+};
+
+let button = document.getElementById("enter");
+let input = document.getElementById("palindrome");
 
 
-
-palindrome("eye");
+button.addEventListener("click", function() {
+	if (input.value.length > 0){
+		palindrome(input.value)
+	}
+});
